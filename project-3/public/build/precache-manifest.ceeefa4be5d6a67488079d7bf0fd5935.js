344b17759f6b38f41058ae5a2e3c2355434aca93
Joey Billingsley <jbillings2082@gmail.com>
2020-06-30 20:29:30 -0400

    First Push

---
+++ project-3/public/build/precache-manifest.ceeefa4be5d6a67488079d7bf0fd5935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131e57b3561a6b5a58a2132325ddbb8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bac0fda49beace13128",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "31cfd3740ab6cee250f0",
+    "url": "/static/js/2.06880d15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06880d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bac0fda49beace13128",
+    "url": "/static/js/main.6d3395a4.chunk.js"
+  },
+  {
+    "revision": "53d69f4a0c140d0ca5b5",
+    "url": "/static/js/runtime-main.f2447cd3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
